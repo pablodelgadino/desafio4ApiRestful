fffdeb2ed30a01095e22268ba919c3c881375b45
Pablo Delgadino <delgadinop@gmail.com>
2022-11-15 20:43:19 -0300

    Desafio 4 Apirestfull

---
+++ products.js
@@ -0,0 +1,16 @@
+let products = [
+    {
+        id: 1,
+        title: "Torta Cabsha",
+        price: 1800,
+        thumbnail: "https://cocinaconlaura.es/wp-content/uploads/2015/03/mini_tarta-cabsha-e1596986492354.jpg"
+    },
+    {
+        id: 2,
+        title: "Lemon Pie",
+        price: 2200,
+        thumbnail: "https://www.divinacocina.es/wp-content/uploads/2015/09/tarta-de-limon-merengue-lemon-pie.jpg"
+    }
+]
+
+module.exports = products
